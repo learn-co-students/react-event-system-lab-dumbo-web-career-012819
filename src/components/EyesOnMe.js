5ab9409b99a311c447e4c7d96b8eb6f3a4d52e71
Cristian Leiva <cleiva94.cl@gmail.com>
2019-04-02 15:59:34 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+export default class EyesOnMe extends React.Component {
+  handleBlur = () => {
+    console.log('Hey! Eyes on me!')
+  }
+
+  handleFocus = () => [
+    console.log('Good!')
+  ]
+  render () {
+    return (
+      <button onBlur={this.handleBlur} onFocus={this.handleFocus} ></button>
+    )
+  }
+}
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+export default class Keypad extends React.Component {
+
+  handleClick = () => {
+    console.log('Entering password...')
+  }
+  render () {
+    return (
+      <div>
+        <input onKeyUp={this.handleClick} type='password' />
+      </div>
+    )
+  }
+}
