287281464a8bcec1dd2ffbaa2898675f3a0ae00d
Maximo Bautista <max.bautista97@gmail.com>
2019-04-04 11:31:10 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+
+import React from 'react'
+const EyesOnMe = () => {
+
+  const focusHandler = () => {
+    console.log('Good!');
+  }
+
+  const blurHandler = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+
+  return(
+    <button value="Button" onFocus={() => focusHandler()} onBlur={() => blurHandler()} > Attention</button>
+  )
+}
+
+export default EyesOnMe;
